9fc278406cd97e1d86f7284ea5346433a0ae2f0d
Daner Yasin <77366878+danerkestey@users.noreply.github.com>
2021-05-01 15:57:26 -0400

    video comp

---
+++ src/videoEmbed/videoEmbed.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles.css";
+import List from "@material-ui/core/List";
+import { Divider, Button } from "@material-ui/core";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import YoutubeEmbed from "./YoutubeEmbed";
+
+class videoEmbedComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  render() {
+    const {};
+  }
+}
