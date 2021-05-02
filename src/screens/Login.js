7c6c69cc417d15ab6d84c293c9838a9981924cb3
saifaldin14 <saifaldindeveloper@gmail.com>
2021-05-02 16:25:50 -0400

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -32,27 +32,75 @@ const Login = () => {
       {/* <Card> */}
       <Card style={{ fontFamily: "Nunito" }}>
         <Card.Body>
-          <h2>Log In</h2>
+          <h1
+            style={{
+              fontFamily: "Nunito",
+              textAlign: "center",
+              fontSize: 24,
+              padding: 10,
+            }}
+          >
+            Log In
+          </h1>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
+            <Form.Group
+              id="email"
+              style={{
+                fontFamily: "Nunito",
+                textAlign: "center",
+                fontSize: 18,
+                padding: 10,
+              }}
+            >
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
-            <Form.Group id="password">
+            <Form.Group
+              id="password"
+              style={{
+                fontFamily: "Nunito",
+                textAlign: "center",
+                fontSize: 18,
+                padding: 10,
+              }}
+            >
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Log In
-            </Button>
+            <div
+              style={{
+                fontFamily: "Nunito",
+                textAlign: "center",
+                fontSize: 18,
+                padding: 10,
+              }}
+            >
+              <Button disabled={loading} className="w-100" type="submit">
+                Log In
+              </Button>
+            </div>
           </Form>
-          <div className="w-100 text-center mt-3">
+          <div
+            style={{
+              fontFamily: "Nunito",
+              textAlign: "center",
+              fontSize: 18,
+              padding: 10,
+            }}
+          >
             <Link to="/forgot-password">Forgot Password?</Link>
           </div>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
+      <div
+        style={{
+          fontFamily: "Nunito",
+          textAlign: "center",
+          fontSize: 18,
+          padding: 10,
+        }}
+      >
         Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
